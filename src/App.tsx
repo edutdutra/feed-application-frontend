691e08a308d710ec8940487a873a9542e8c31ba1
eduardodutra <edutdutra@gmail.com>
2022-11-09 09:29:56 -0300

    fix project

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {Post} from "./components/Post";
+import {Header} from "./components/Header";
+import {Sidebar} from "./components/Sidebar";
+
+import styles from './App.module.css';
+import './global.css';
+
+const posts = [
+    {
+        id: 1,
+        author: {
+            avatarUrl: "https://github.com/edutdutra.png",
+            name: "Eduardo Dutra",
+            role: "Developer"
+        },
+        content: [
+            {type: 'paragraph', content: 'Fala galeraa 👋'},
+            {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+            {type: 'link', content: 'jane.design/doctorcare'},
+        ],
+        publishedAt: new Date('2022-11-05 20:00:00')
+    },
+    {
+        id: 2,
+        author: {
+            avatarUrl: "https://github.com/edutdutra.png",
+            name: "Test Dutra",
+            role: "Student"
+        },
+        content: [
+            {type: 'paragraph', content: 'Fala galeraa 👋'},
+            {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+            {type: 'link', content: 'jane.design/doctorcare'},
+        ],
+        publishedAt: new Date('2022-05-03 20:00:00')
+    },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div>
+            <Header/>
+            <div className={styles.wrapper}>
+                <Sidebar/>
+                <main>
+                    {posts.map(post => {
+                        return (
+                            <Post key={post.id} author={post.author} content={post.content} publishedAt={post.publishedAt} />
+                        )
+                    })}
+                </main>
+            </div>
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+        </div>
+    )
 }
 
 export default App
---
+++ src/components/Comment.tsx
@@ -0,0 +1,47 @@
+import styles from './Comment.module.css';
+import {ThumbsUp, Trash} from "phosphor-react";
+import {Avatar} from "./Avatar";
+import {useState} from "react";
+
+export function Comment({content, onDeleteComment}) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/edutdutra.png"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <time title="31 de Outubro às 08:47" dateTime="2022-10-31 08:47:30">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
